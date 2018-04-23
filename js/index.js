3262bccc130a8fafa5d7b79d7dc87b3a5f1348d9
RobertHale <rthspike@gmail.com>
2018-04-23 18:31:34 -0500

    basic d3 to make a circle

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-$(function() {
-  initFirstChart();
-});
-
-function initFirstChart(){
-  
+function createCircle() {
+    let bodySelection = d3.select("body");
+    let svgSelection = bodySelection.append("svg")
+        .attr("width", 50)
+        .attr("height", 50);
+    let circleSelection = svgSelection.append("circle")
+        .attr("cx", 25)
+        .attr("cy", 25)
+        .attr("r", 25)
+        .style("fill", "purple");
 }
